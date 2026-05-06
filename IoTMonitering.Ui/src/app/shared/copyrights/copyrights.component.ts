d6cdcef219a9b4efdb6f7f715eba19cb06c92d43
devendra <devendra_dewangan@outlook.com>
2026-05-06 17:41:26 +0530

    modified styling

--- IoTMonitering.Ui/src/app/shared/copyrights/copyrights.component.ts
+++ IoTMonitering.Ui/src/app/shared/copyrights/copyrights.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-copyrights',
   templateUrl: './copyrights.component.html',
-  styleUrl: './copyrights.component.css'
+  styleUrl: './copyrights.component.css',
+  host: {
+    class: 'w-3xl bg-gray-800 text-white p-4'
+  }
 })
 export class CopyrightsComponent {
 
